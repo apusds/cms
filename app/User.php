8361b2d95dd8ce8a0c44a951771e147d26f2f338
InspectorGadget™ <igadget28@gmail.com>
2019-08-25 00:30:15 +0800

    roles table

--- app/User.php
+++ app/User.php
@@ -25,15 +25,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function role() {
+        return $this->belongsTo('App\Role');
+    }
 }
