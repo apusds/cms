a7ae8fbe0e3de8602098a417f03706c1daaebffa
InspectorGadget™ <igadget28@gmail.com>
2019-08-25 00:26:22 +0800

    file structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('role_id')->index()->unsigned()->nullable();
+            $table->integer('is_active')->default(0);
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
