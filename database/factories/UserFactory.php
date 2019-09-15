41b914b4d54bbf69c7c362611833116dd1c6ecfa
InspectorGadget™ <igadget28@gmail.com>
2019-09-15 11:34:56 +0800

    added Faker (Factory)

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\User;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'username' => $faker->userName,
+        'email' => $faker->unique()->safeEmail,
+        'role_id' => 1,
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    ];
+});
