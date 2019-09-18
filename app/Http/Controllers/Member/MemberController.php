33d06354dd11fb7aa072d661b0e66c5ca8ac2929
InspectorGadget™ <igadget28@gmail.com>
2019-09-18 22:43:30 +0800

    created Member model, Controller + Model!

---
+++ app/Http/Controllers/Member/MemberController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Member;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MemberController extends Controller
+{
+    //
+}
---
+++ database/migrations/2019_09_18_144258_create_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            // TODO
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
