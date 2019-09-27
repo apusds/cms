87ed4494027a99fce91cbd39482a8808764cb5be
InspectorGadget™ <igadget28@gmail.com>
2019-09-27 20:27:52 +0800

    Changed the view of the Team (Admin)

--- resources/views/admin/teams/index.blade.php
+++ resources/views/admin/teams/index.blade.php
@@ -16,8 +16,8 @@
         @if (count(\App\Committee::all()) > 0)
             @foreach (\App\Committee::all() as $committee)
                 <div class="col-sm-12 col-lg-3" style="width: 18rem;">
-                    <div class="card">
-                        <img class="card-img-top" src="{{ asset(env('PUBLIC_PATH') . '/committee/' . $committee->file) }}" alt="Image not loaded">
+                    <div class="card" style="width: 350px !important;">
+                        <img class="card-img-top img-fluid" src="{{ asset(env('PUBLIC_PATH') . '/committee/' . $committee->file) }}" alt="Image not loaded" style="height: 350px !important;">
                         <div class="card-body">
                             <h5 class="card-title">{{ $committee->name }}</h5>
                             <p class="card-text">{{ $committee->role }}</p>
