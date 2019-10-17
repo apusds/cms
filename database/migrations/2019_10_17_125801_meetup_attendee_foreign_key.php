d99f3a930d819af6305d0b2906bf1e93c9af02d2
kklee998 <kahkinlee998@gmail.com>
2019-10-17 13:18:22 +0800

    initial foreign key constraint commit

---
+++ database/migrations/2019_10_17_125801_meetup_attendee_foreign_key.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MeetupAttendeeForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('members', function (Blueprint $table) {
+
+            $table->unique('student_id');
+
+        });
+
+        Schema::table('meetups', function (Blueprint $table) {
+            $table->unique('title');
+        });
+
+        Schema::table('meetup_attendees', function (Blueprint $table) {
+            $table->foreign('student_id')->references('student_id')->on('members');
+            $table->foreign('meetup_title')->references('title')->on('meetups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
