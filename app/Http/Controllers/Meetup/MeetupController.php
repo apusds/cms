fdda86e5dcfabc598fce06cb9b90d19e9528247b
kklee998 <kahkinlee998@gmail.com>
2019-10-26 19:28:40 +0800

    updated meetup controller to use Eloquent instead of Query Builder

--- app/Http/Controllers/Meetup/MeetupController.php
+++ app/Http/Controllers/Meetup/MeetupController.php
@@ -19,28 +19,27 @@ class MeetupController extends Controller
 
         if (!$validate) return back()->with('error', 'Malformed Request!');
 
-        $result = DB::table(env('DB_MEETUPS'))
-            ->insertGetId([
-                'title' => trim($request->input('title')),
-                'description' => trim($request->input('description')),
-                'location' => trim($request->input('location')),
-                'event_start' => Carbon::make($request->input('event_start'))->format('Y-m-d H:i:s'),
-                'event_end' => Carbon::make($request->input('event_end'))->format('Y-m-d H:i:s'),
-                'created_at' => new \DateTime()
-            ]);
+        $result = new Meetup;
+        $result->title = trim($request->input('title'));
+        $result->description = trim($request->input('description'));
+        $result->event_start = Carbon::make($request->input('event_start'))->format('Y-m-d H:i:s');
+        $result->event_end = Carbon::make($request->input('event_end'))->format('Y-m-d H:i:s');
+        $result->location = trim($request->input('location'));
+        $result->save();
+
         if ($request->input('isActive')) {
 
             if (ActiveMeetup::first()) {
-                ActiveMeetup::first()->update(['event_id' => $result]);
+                ActiveMeetup::first()->update(['event_id' => $result->id]);
 
             }else{
                 $new_active = new ActiveMeetup;
-                $new_active->event_id = $result;
+                $new_active->event_id = $result->id;
                 $new_active->save();
             }
 
-
         }
+
 
         return $result
             ? redirect(route('dashboard.meetups'))->with('message', 'Done! Meetup created! ')
@@ -57,16 +56,13 @@ class MeetupController extends Controller
 
         if (!$validate) return back()->with('error', 'Malformed POST, please check your Input.');
 
-        $result = DB::table(env('DB_MEETUPS'))
-            ->where('id', $id)
-            ->update([
-                'title' => trim($request->input('title')),
-                'description' => trim($request->input('description')),
-                'location' => trim($request->input('location')),
-                'event_start' => Carbon::make($request->input('event_start'))->format('Y-m-d H:i:s'),
-                'event_end' => Carbon::make($request->input('event_end'))->format('Y-m-d H:i:s'),
-                'created_at' => new \DateTime()
-            ]);
+        $result = Meetup::find($id);
+        $result->title = trim($request->input('title'));
+        $result->description = trim($request->input('description'));
+        $result->event_start = Carbon::make($request->input('event_start'))->format('Y-m-d H:i:s');
+        $result->event_end = Carbon::make($request->input('event_end'))->format('Y-m-d H:i:s');
+        $result->location = trim($request->input('location'));
+        $result->save();
 
         if ($request->input('isActive')) {
             if (ActiveMeetup::first()) {
@@ -85,7 +81,7 @@ class MeetupController extends Controller
     }
 
     public function delete($id) {
-        $result = Meetup::all()->find($id);
+        $result = Meetup::find($id);
         if (!$result) return view('errors.500');
 
         try {
@@ -97,7 +93,7 @@ class MeetupController extends Controller
     }
 
     public function deactivate() {
-        $result = ActiveMeetup::all()->first();
+        $result = ActiveMeetup::first();
         if (!$result) return view('errors.404');
 
         try {
