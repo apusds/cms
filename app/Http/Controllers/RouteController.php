62a18bb3344b1301308c7dcf0ad966ba003c59d9
euseanwoon2016 <euseanwoon2016@github.com>
2019-12-13 16:21:25 +0800

    updated register

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
-use App\{Committee,
+use App\{
+    Committee,
     Event,
     Meetup,
     ActiveMeetup,
@@ -12,12 +13,14 @@ use App\{Committee,
     Member,
     Role,
     User,
-    Website};
+    Website
+};
 
 class RouteController extends Controller
 {
 
-    public function home() {
+    public function home()
+    {
         return view('layouts.website.index', [
             'activeEvents' => app(EventController::class)->getActiveEvents(),
             'expiredEvents' => app(EventController::class)->getExpiredEvents(),
@@ -26,36 +29,48 @@ class RouteController extends Controller
         ]);
     }
 
-    public function showCheckin() {
+    public function showRegister()
+    {
+        return view('layouts.website.register', ['data' => Website::all()->find(1)]);
+    }
+
+    public function showCheckin()
+    {
         $result = (ActiveMeetup::first()) ? ActiveMeetup::first()->meetup : '';
         return view('layouts.website.checkin', ['data' => $result]);
     }
 
-    public function showAdminLogin() {
+    public function showAdminLogin()
+    {
         return view('login.index');
     }
 
-    public function showDashboard() {
+    public function showDashboard()
+    {
         return view('admin.index', [
             'joinedToday' => app(MemberController::class)->joinedToday()
         ]);
     }
 
-    public function showProfile() {
+    public function showProfile()
+    {
         return view('admin.profile.index');
     }
 
     // [Events]
 
-    public function showEvents() {
+    public function showEvents()
+    {
         return view('admin.events.index');
     }
 
-    public function showEventCreate() {
+    public function showEventCreate()
+    {
         return view('admin.events.create');
     }
 
-    public function showEventEdit($id) {
+    public function showEventEdit($id)
+    {
         return view('admin.events.edit', ['data' => Event::all()->find($id)]);
     }
 
@@ -63,22 +78,26 @@ class RouteController extends Controller
 
     // [Meetups]
 
-    public function showMeetups() {
+    public function showMeetups()
+    {
         return view('admin.meetups.index');
     }
 
-    public function showMeetupCreate() {
+    public function showMeetupCreate()
+    {
         return view('admin.meetups.create');
     }
 
-    public function showMeetupEdit($id) {
+    public function showMeetupEdit($id)
+    {
         return view('admin.meetups.edit', ['data' => Meetup::all()->find($id)]);
     }
 
     // End [Meetups]
 
     // [Meetup Attendees]
-    public function showMeetupAttendees($id) {
+    public function showMeetupAttendees($id)
+    {
         return view('admin.meetups.attendees', ['meetup' => Meetup::find($id)]);
     }
 
@@ -86,15 +105,18 @@ class RouteController extends Controller
 
     // [Users]
 
-    public function showUsers() {
+    public function showUsers()
+    {
         return view('admin.users.index');
     }
 
-    public function showUserCreate() {
+    public function showUserCreate()
+    {
         return view('admin.users.create');
     }
 
-    public function showUserEdit($id) {
+    public function showUserEdit($id)
+    {
         if ($id == Auth::user()->id) return back()->with('error', 'You cannot edit the details of yourself!');
         if (!(User::all()->find($id))) return back()->with('error', 'Invalid ID, User not found!'); // If the User does not exist, break the request and page render.
 
@@ -105,26 +127,30 @@ class RouteController extends Controller
 
     // Serve [Event Page]
 
-    public function showEvent($name) {
+    public function showEvent($name)
+    {
         $event = Event::all()->where('identifier', trim(strtolower($name)))->first();
         if (!$event) return view('errors.404');
 
         return view('errors.404');
-//        return view('layouts.website.event', ['data' => $event]);
+        //        return view('layouts.website.event', ['data' => $event]);
     }
 
     // Serve [Event Page]
 
     // [Pages]
-    public function showPages() {
+    public function showPages()
+    {
         return view('admin.pages.index');
     }
 
-    public function showPageCreate() {
+    public function showPageCreate()
+    {
         return view('admin.pages.create');
     }
 
-    public function showPageEdit($id) {
+    public function showPageEdit($id)
+    {
         if (!(Page::all()->find($id))) return back()->with('error', 'This Page is not found!');
 
         return view('admin.pages.edit', ['page' => Page::all()->find($id)]);
@@ -133,11 +159,13 @@ class RouteController extends Controller
     // End [Pages]
 
     // [Templates]
-    public function showTemplates() {
+    public function showTemplates()
+    {
         return view('admin.templates.index');
     }
 
-    public function showTemplateEdit($id) {
+    public function showTemplateEdit($id)
+    {
         if (!(Template::all()->find($id))) return back()->with('error', 'This Template is not found!');
 
         return view('admin.templates.edit', ['template' => Template::all()->find($id)]);
@@ -146,11 +174,13 @@ class RouteController extends Controller
     // End [Templates]
 
     // [Roles]
-    public function showRoles() {
+    public function showRoles()
+    {
         return view('admin.roles.index');
     }
 
-    public function showRoleEdit($id) {
+    public function showRoleEdit($id)
+    {
         if ($id == 1) return back()->with('error', 'You cannot edit this role!');
         if (!(Role::all()->find($id))) return back()->with('error', 'Invalid Role, Role not found!');
 
@@ -160,47 +190,56 @@ class RouteController extends Controller
     // End [Roles]
 
     // [Members]
-    public function showMembers() {
+    public function showMembers()
+    {
         return view('admin.members.index');
     }
 
-    public function showEditMember($id) {
+    public function showEditMember($id)
+    {
         if (!(Member::all()->find($id))) return back()->with('error', 'Member not found!');
         return view('admin.members.edit', ['data' => Member::all()->find($id)]);
     }
     // End [Members]
 
     // [Emailer]
-    public function showEmailer() {
+    public function showEmailer()
+    {
         return view('admin.emailer.index');
     }
     // End [Emailer]
 
     // [Global Settings]
-    public function showWebsite() {
+    public function showWebsite()
+    {
         return view('admin.website.index', [
             'data' => Website::all()->find(1)
         ]);
     }
 
-    public function showTeams() {
+    public function showTeams()
+    {
         return view('admin.teams.index');
     }
 
-    public function showTeamsCreate() {
+    public function showTeamsCreate()
+    {
         return view('admin.teams.create');
     }
 
-    public function showTeamsEdit($id) {
+    public function showTeamsEdit($id)
+    {
         if (!(Committee::all()->find($id))) return back()->with('error', 'This Member is not found!');
         return view('admin.teams.edit', ['data' => Committee::all()->find($id)]);
     }
 
-    public function showGallery() {
+    public function showGallery()
+    {
         return view('admin.gallery.index');
     }
 
-    public function showGalleryCreate() {
+    public function showGalleryCreate()
+    {
         return view('admin.gallery.create', [
             'events' => Event::all()
         ]);
---
+++ resources/views/layouts/website/register.blade.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>{{ $data->title }}</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="{{ $data->keyword }}" name="keywords">
+  <meta content="APU Student Developer Society" name="description">
+
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148897965-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-148897965-1');
+  </script>
+
+  <!-- Favicons -->
+  <link href="{{ asset('img/favicon.png') }}" rel="icon">
+  <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+  <!-- Faggot<li><a href="#partners">Partners</a></li> Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,600,700,700i|Montserrat:300,400,500,600,700"
+    rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="{{ asset('lib/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="{{ asset('lib/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('lib/animate/animate.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('lib/ionicons/css/ionicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+
+  <!-- Register Stylesheet File -->
+  <link href="{{ asset('css/register/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+  <div class="container">
+    <div id="membershipForm">
+      <div class="content rounded">
+        <h4>Membership Form</h4>
+        <span><b><i>Note: Open to APU Students & Alumni only</i></b></span>
+
+        <hr />
+
+        <form method="POST" action="{{ route('membership.post') }}">
+          {{ csrf_field() }}
+
+          <div class="form-group">
+            <label for="email">Email Address <span class="red">*</span></label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email" required>
+          </div>
+
+          <div class="form-group">
+            <label for="name">Name <span class="red">*</span></label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" required>
+          </div>
+
+          <div class="form-group">
+            <label for="mobile">Mobile <span class="red">*</span></label>
+            <input type="text" class="form-control" id="mobile" name="mobile" placeholder="Enter Mobile Number"
+              required>
+          </div>
+
+          <div class="form-group">
+            <label for="tp">TP Number <span class="red">*</span></label>
+            <input type="text" class="form-control" id="tp" name="tp" placeholder="Enter TP Number" required>
+            <small style="padding-left: 15px;" class="red" id="tpError"></small>
+          </div>
+
+          <div class="form-group">
+            <label for="intake">Intake <span class="red">*</span></label>
+            <input type="text" class="form-control" id="intake" name="intake" placeholder="Enter Intake" required>
+            <small style="padding-left: 15px;" class="red" id="intakeError"></small>
+          </div>
+
+          <label for="gender">Gender? <span class="red">*</span></label>
+
+          <div>
+            <div class="material-radio">
+              <input class="form-check-input" type="checkbox" value="male" id="male" name="gender">
+              <label class="form-check-label" for="male">
+                Male
+              </label>
+            </div>
+
+            <div class="material-radio">
+              <input class="form-check-input" type="checkbox" value="female" id="female" name="gender">
+              <label class="form-check-label" for="female">
+                Female
+              </label>
+            </div>
+          </div>
+
+          <label for="skills">Skills <span class="red">*</span></label>
+
+          <div class="row">
+            <div class="material-check col">
+              <input class="form-check-input" type="checkbox" value="beginner" id="beginner" name="skills[]">
+              <label class="form-check-label" for="beginner">
+                Beginner
+              </label>
+            </div>
+
+            <div class="material-check col">
+              <input class="form-check-input" type="checkbox" value="web dev" id="web dev" name="skills[]">
+              <label class="form-check-label" for="web dev">
+                Web Dev
+              </label>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="material-check col">
+              <input class="form-check-input" type="checkbox" value="backend" id="backend" name="skills[]">
+              <label class="form-check-label" for="backend">
+                Backend
+              </label>
+            </div>
+
+            <div class="material-check col">
+              <input class="form-check-input" type="checkbox" value="android" id="android" name="skills[]">
+              <label class="form-check-label" for="android">
+                Android
+              </label>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="material-check col">
+              <input class="form-check-input" type="checkbox" value="deployment" id="deployment" name="skills[]">
+              <label class="form-check-label" for="deployment">
+                Deployment
+              </label>
+            </div>
+
+            <div class="material-check col">
+              <input class="form-check-input" type="checkbox" value="database" id="database" name="skills[]">
+              <label class="form-check-label" for="database">
+                Database
+              </label>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="material-check col">
+              <input class="form-check-input" type="checkbox" value="networking" id="networking" name="skills[]">
+              <label class="form-check-label" for="networking">
+                Networking
+              </label>
+            </div>
+
+            <div class="material-check col invisible">
+              <input class="form-check-input">
+              <label class="form-check-label">
+              </label>
+            </div>
+
+          </div>
+
+          <br>
+
+          <label for="find-us">How did you find us? <span class="red">*</span></label>
+
+          <div>
+            <div class="material-radio">
+              <input class="form-check-input" type="checkbox" value="0" id="facebook" name="check">
+              <label class="form-check-label" for="facebook">
+                Social Media
+              </label>
+            </div>
+
+            <div class="material-radio">
+              <input class="form-check-input" type="checkbox" value="1" id="heard" name="check">
+              <label class="form-check-label" for="heard">
+                Heard From Friend
+              </label>
+            </div>
+
+            <div class="material-radio">
+              <input class="form-check-input" type="checkbox" value="2" id="attended" name="check">
+              <label class="form-check-label" for="attended">
+                Attended Event/Workshop
+              </label>
+            </div>
+          </div>
+          <hr>
+          <button type="submit" id="submitBtn" class="btn btn-success">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div id="preloader"></div>
+
+  <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('lib/jquery/jquery-migrate.min.js') }}"></script>
+  <script src="{{ asset('lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('lib/easing/easing.min.js') }}"></script>
+  <script src="{{ asset('lib/mobile-nav/mobile-nav.js') }}"></script>
+  <script src="{{ asset('lib/wow/wow.min.js') }}"></script>
+  <script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
+  <script src="{{ asset('lib/counterup/counterup.min.js') }}"></script>
+  <script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('lib/isotope/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ asset('lib/lightbox/js/lightbox.min.js') }}"></script>
+
+  <script src="{{ asset('js/main/main.js') }}"></script>
+
+</body>
+
+</html>
