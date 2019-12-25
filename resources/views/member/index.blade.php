0721c3a31b00315b8a20a768e357122ba769aebc
InspectorGadget™ <igadget28@gmail.com>
2019-12-25 21:46:54 +0800

    updated the current url on dev environment

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -6,7 +6,7 @@
     <h2>Hi sir</h2>
     <a
         class="{{ Session::has('ticket') ? 'btn btn-danger' : 'btn btn-primary' }}"
-        href="https://cas.apiit.edu.my/cas/login?service=http://127.0.0.1:8000/api/cas/auth">
+        href="https://cas.apiit.edu.my/cas/login?service={{ Request::fullUrl() }}/api/cas/auth">
         {{ Session::has('ticket') ? Session::get('ticket') : 'Link to CAS' }}
     </a>
 @stop
