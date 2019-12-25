c456f2dd32107fa2ef8070d51b8fc36e28f871e1
InspectorGadget™ <igadget28@gmail.com>
2019-12-25 21:51:01 +0800

    switch to request::root()

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -6,7 +6,7 @@
     <h2>Hi sir</h2>
     <a
         class="{{ Session::has('ticket') ? 'btn btn-danger' : 'btn btn-primary' }}"
-        href="https://cas.apiit.edu.my/cas/login?service={{ Request::fullUrl() }}/api/cas/auth">
+        href="https://cas.apiit.edu.my/cas/login?service={{ Request::root() }}/api/cas/auth">
         {{ Session::has('ticket') ? Session::get('ticket') : 'Link to CAS' }}
     </a>
 @stop
