c03689b53b8a55b7b334ef61bcbe7dbedad15b9f
InspectorGadget <igadget28@gmail.com>
2019-12-26 15:43:16 +0800

    Testing CAS

--- app/Http/Controllers/API/CASController.php
+++ app/Http/Controllers/API/CASController.php
@@ -22,7 +22,7 @@ class CASController extends Controller
         $this->casST = $request->get('ticket');
         $response = $this->getHttp()->request('GET', $this->casUrl . "/cas/p3/serviceValidate?service={{ $baseUrl }}/api/cas/auth&ticket={$this->casST}");
         // $response = $this->getHttp()->request("GET", "https://api.apiit.edu.my/student/profile?ticket={$this->casST}");
-        dd($response);
+        dd($response->getBody());
         return redirect(route('member.dashboard'))->with('ticket', $this->casST);
    }
 }
