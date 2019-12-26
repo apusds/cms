7ded1dc0e4b7bc20864b67cde248d5a40f0350b8
InspectorGadget <igadget28@gmail.com>
2019-12-26 15:44:49 +0800

    Added Content-type for testing

--- app/Http/Controllers/API/CASController.php
+++ app/Http/Controllers/API/CASController.php
@@ -20,9 +20,13 @@ class CASController extends Controller
         $baseUrl = \Illuminate\Support\Facades\Request::root();
         if (!$request->get('ticket')) return redirect($this->casUrl . "/cas/login?service={{ $baseUrl }}/api/cas/auth");
         $this->casST = $request->get('ticket');
-        $response = $this->getHttp()->request('GET', $this->casUrl . "/cas/p3/serviceValidate?service={{ $baseUrl }}/api/cas/auth&ticket={$this->casST}");
+        $response = $this->getHttp()->request('GET', $this->casUrl . "/cas/p3/serviceValidate?service={{ $baseUrl }}/api/cas/auth&ticket={$this->casST}", [
+            'headers' => [
+                'Content-type' => 'application/x-www-form-urlencoded'
+            ]
+        ]);
         // $response = $this->getHttp()->request("GET", "https://api.apiit.edu.my/student/profile?ticket={$this->casST}");
-        dd($response->getBody());
+        dd($response->getHeaders());
         return redirect(route('member.dashboard'))->with('ticket', $this->casST);
    }
 }
