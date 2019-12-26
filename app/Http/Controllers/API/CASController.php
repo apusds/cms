9c5a039ef9dc03f72e13ce728aba9a13f2bcb949
InspectorGadget <igadget28@gmail.com>
2019-12-26 15:58:55 +0800

    Added CAS Ticket Validation

--- app/Http/Controllers/API/CASController.php
+++ app/Http/Controllers/API/CASController.php
@@ -18,15 +18,10 @@ class CASController extends Controller
 
    public function getStudentProfile(Request $request) {
         $baseUrl = \Illuminate\Support\Facades\Request::root();
-        if (!$request->get('ticket')) return redirect($this->casUrl . "/cas/login?service={{ $baseUrl }}/api/cas/auth");
+        if (!$request->get('ticket')) return redirect($this->casUrl . "/cas/login?service=$baseUrl/api/cas/auth");
         $this->casST = $request->get('ticket');
-        $response = $this->getHttp()->request('GET', $this->casUrl . "/cas/p3/serviceValidate?service={{ $baseUrl }}/api/cas/auth&ticket={$this->casST}", [
-            'headers' => [
-                'Content-type' => 'application/x-www-form-urlencoded'
-            ]
-        ]);
-        // $response = $this->getHttp()->request("GET", "https://api.apiit.edu.my/student/profile?ticket={$this->casST}");
-        dd($response->getHeaders());
+        $response = $this->getHttp()->request('GET', $this->casUrl . "/cas/p3/serviceValidate?service=$baseUrl/api/cas/auth&ticket={$this->casST}");
+        dd($response->getBody()->getContents());
         return redirect(route('member.dashboard'))->with('ticket', $this->casST);
    }
 }
