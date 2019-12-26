10d5861b32f866857238b74dbcc2f1c1978f2353
InspectorGadget <igadget28@gmail.com>
2019-12-26 16:16:33 +0800

    Fix Ticket Validation

--- app/Http/Controllers/API/CASController.php
+++ app/Http/Controllers/API/CASController.php
@@ -11,6 +11,7 @@ class CASController extends Controller
 
     private $casUrl = 'https://cas.apiit.edu.my';
     private $casST = '';
+    private $casData;
 
     private function getHttp(): Client {
         return new Client();
@@ -20,8 +21,20 @@ class CASController extends Controller
         $baseUrl = \Illuminate\Support\Facades\Request::root();
         if (!$request->get('ticket')) return redirect($this->casUrl . "/cas/login?service=$baseUrl/api/cas/auth");
         $this->casST = $request->get('ticket');
-        $response = $this->getHttp()->request('GET', $this->casUrl . "/cas/p3/serviceValidate?service=$baseUrl/api/cas/auth&ticket={$this->casST}");
-        dd($response->getBody()->getContents());
-        return redirect(route('member.dashboard'))->with('ticket', $this->casST);
+
+        $response = $this->getHttp()->request('GET', $this->casUrl . "/cas/p3/serviceValidate?service=$baseUrl/api/cas/auth&ticket={$this->casST}&format=json", [
+            'headers' => [
+                'Content-type' => 'application/x-www-form-urlencoded'
+            ]
+        ]);
+
+        $this->casData = json_decode($response->getBody()->getContents());
+        
+        dd($this->casData);
+
+        return redirect(route('member.dashboard'))->with([
+            'ticket', $this->casST,
+            'data' => $this->casData
+        ]);
    }
 }
