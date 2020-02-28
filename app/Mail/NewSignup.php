5e4ccc6d609c6bc94fa188810d0127d083c308fc
InspectorGadget™ <igadget28@gmail.com>
2020-02-28 23:24:50 +0800

    Added Mailer

---
+++ app/Mail/NewSignup.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NewSignup extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $name;
+
+    public function __construct(String $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Welcome to APUSDS')
+            ->view('emails.members.signup')
+            ->with([
+                'name' => $this->name
+            ]);
+    }
+}
